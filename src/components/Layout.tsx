23bcbe16cf19b746a204eab90d3e0d29013d535b
Edgar <edgarmontenegro321@gmail.com>
2026-06-14 04:10:46 -0300

    ✨ feat: implement layout and navigation structure

---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import Navbar from './Navbar'
+
+interface LayoutProps {
+    children: React.ReactNode
+}
+
+const Layout = ({ children }: LayoutProps) => {
+    return (
+        <>
+            <Navbar />
+            <main>{children}</main>
+            {/* Aquí luego agregaremos el Footer */}
+        </>
+    )
+}
+
+export default Layout
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <main>
+            <h1>Bienvenido a nuestra Tienda</h1>
+            <p>Esta es nuestra página principal</p>
+        </main>
+    )
+}
+
+export default Home
