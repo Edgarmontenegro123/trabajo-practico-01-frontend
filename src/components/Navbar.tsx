4035ac225a1a98a702f785740c44d7dd5d706107
Edgar <edgarmontenegro321@gmail.com>
2026-06-14 05:05:15 -0300

    ✨ feat: add mobile responsive hamburger menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,31 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleMenu = () => setIsOpen(!isOpen)
+
     return (
         <nav className='navbar'>
             <div className='logo'>Mi Tienda</div>
-            <ul>
-                <li><Link to='/'>Inicio</Link></li>
-                <li><Link to='/tienda'>Nuestro Café</Link></li>
-                <li><Link to='/tutoriales'>Tutoriales</Link></li>
-                <li><Link to='/menu-tienda'>Nuestro Menú</Link></li>
-                <li><Link to='/trabaja-en-tdc'>Trabajá con Nosotros</Link></li>
-                <li><Link to='/franquicias'>Franquicias</Link></li>
-                <li><Link to='/contacto'>Contacto</Link></li>
+
+            {/* Botón hamburguesa (visible solo en móvil) */}
+            <div className={`hamburger ${isOpen ? 'active' : ''}`} onClick={toggleMenu}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+
+            <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
+                <li><Link to='/' onClick={toggleMenu}>Inicio</Link></li>
+                <li><Link to='/tienda' onClick={toggleMenu}>Nuestro Té</Link></li>
+                <li><Link to='/tutoriales' onClick={toggleMenu}>Tutoriales</Link></li>
+                <li><Link to='/menu-tienda' onClick={toggleMenu}>Nuestro Menú</Link></li>
+                <li><Link to='/trabaja-en-tdc' onClick={toggleMenu}>Trabajá con Nosotros</Link></li>
+                <li><Link to='/franquicias' onClick={toggleMenu}>Franquicias</Link></li>
+                <li><Link to='/contacto' onClick={toggleMenu}>Contacto</Link></li>
             </ul>
         </nav>
     )
