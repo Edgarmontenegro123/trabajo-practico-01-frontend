3f55949d42f3f9833617e24f670c71fcb7b6963e
Edgar <edgarmontenegro321@gmail.com>
2026-06-18 00:37:07 -0300

    ✨ feat: integrate FontAwesome icons in footer and add social media links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,13 +9,11 @@ const Footer = () => {
                     <h3>Mundo Mate</h3>
                     <p>El ritual del mate, directo a tu mano.</p>
                 </div>
-
                 <div className='footer-col'>
                     <h4>CONTENIDOS</h4>
                     <Link to='/'>Inicio</Link>
                     <Link to='/productos'>Nuestros Mates</Link>
                 </div>
-
                 <div className='footer-col'>
                     <h4>NECESITÁS AYUDA</h4>
                     <Link to='/tutoriales'>Tutoriales</Link>
@@ -23,12 +21,25 @@ const Footer = () => {
                     <Link to='/franquicias'>Franquicias</Link>
                     <Link to='/contacto'>Contacto</Link>
                 </div>
-
                 <div className='footer-col'>
                     <h4>SUSCRIBITE</h4>
                     <div className='newsletter'>
                         <input type='email' placeholder='Tu email' />
                         <button>SUSCRIBIRME</button>
+                    </div>
+                </div>
+                <div className='footer-col'>
+                    <h4>SEGUINOS</h4>
+                    <div className='social-links'>
+                        <a href='https://instagram.com' target='_blank' rel='noopener noreferrer'>
+                            <i className='fab fa-instagram'></i>
+                        </a>
+                        <a href='https://facebook.com' target='_blank' rel='noopener noreferrer'>
+                            <i className='fab fa-facebook'></i>
+                        </a>
+                        <a href='https://twitter.com' target='_blank' rel='noopener noreferrer'>
+                            <i className='fab fa-twitter'></i>
+                        </a>
                     </div>
                 </div>
             </div>
