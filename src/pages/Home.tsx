add59ecd1b70b0895ffdb050c322f21e78a8f8c9
Edgar <edgarmontenegro321@gmail.com>
2026-06-19 07:37:13 -0300

    🚀 feat: implement navigation to products page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,19 @@
-import HeroCarousel from '../components/HeroCarousel';
+import {Link} from 'react-router-dom'
+import HeroCarousel from '../components/HeroCarousel'
 import './Home.css'
 
 const Home = () => {
+
     return (
         <main className='home'>
             <HeroCarousel />
             <section className='contenido-extra'>
                 <h1>El ritual de cada día</h1>
                 <p>Descubrí mates artesanales...</p>
-                <button className='cta-button'>Explorar Nuestros Mates</button>
+                <Link
+                    to='/productos'
+                    className='cta-button'
+                >Explorar Nuestros Mates</Link>
             </section>
         </main>
     )
